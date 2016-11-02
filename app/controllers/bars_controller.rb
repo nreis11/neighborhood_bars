b1f310aecb586d8caa88bdd2ac52572033fb3284
Nick Reis <email": null>
2016-11-01 17:41:05 -0700

    add form partial

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,9 +1,27 @@
 class BarsController < ApplicationController
 
-  before_action :set_bars, only: [:show, :edit, :update, :destroy]
+  before_action :set_bar, only: [:show, :edit, :update, :destroy]
+  before_action :set_neighborhood
 
   def index
     @neighborhood = Neighborhood.find(params[:neighborhood_id])
     @bars = @neighborhood.bars
+  end
+
+  def show
+  end
+
+  def new
+    @bar = @neighborhood.bars.new
+  end
+
+  private
+
+  def set_neighborhood
+    @neighborhood = Neighborhood.find(params[:neighborhood_id])
+  end
+
+  def set_bar
+    @bar = Bar.find(params[:id])
   end
 end
